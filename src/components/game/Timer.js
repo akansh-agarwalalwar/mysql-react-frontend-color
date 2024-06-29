6bfe43fbd3ea1c0093e703f8ad5d2fb56e03823f
Akansh Agarwal <66241705+akansh-agarwalalwar@users.noreply.github.com>
2024-06-29 09:56:13 +0530

    Update Timer.js

--- src/components/game/Timer.js
+++ src/components/game/Timer.js
@@ -9,7 +9,6 @@ import UserContext from "../login/UserContext";
 import EveryOneOrder from "./EveryOneOrder";
 import axios from "axios";
 
-
 function Timer() {
   const { user, setUser, fetchUserData } = useContext(UserContext);
   const [time, setTime] = useState(30);
@@ -43,7 +42,7 @@ function Timer() {
         const timeResponse = await axios.get(
           "https://color-server.onrender.com/period-time"
         );
-        setTime((timeResponse.data.countdown || 30)-3);
+        setTime((timeResponse.data.countdown || 30) - 3);
       } catch (error) {
         console.error("Error fetching initial period and time:", error);
       }
@@ -138,12 +137,6 @@ function Timer() {
     }
   }, [time]);
 
-  // useEffect(() => {
-  //   if (time === 0) {
-  //     setLastTableData(newBets); // Save the current bets to lastTableData when the timer resets
-  //   }
-  // }, [time]);
-
   const savePeriodToDatabase = async (newPeriod) => {
     try {
       await axios.post("https://color-server.onrender.com/period-timer/post", {
@@ -173,18 +166,21 @@ function Timer() {
       title: "Red",
       color: "red",
       icon: <FaHorseHead style={{ color: "#FF0000" }} />,
+      ratio: "1:2",
       values: [1, 3, 7, 9],
     },
     {
       title: "Violet",
       color: "purple",
       icon: <FaHorseHead style={{ color: "#800080" }} />,
+      ratio: "1:4.5",
       values: [0, 5],
     },
     {
       title: "Green",
       color: "green",
       icon: <FaHorseHead style={{ color: "#00FF00" }} />,
+      ratio: "1:2",
       values: [2, 4, 6, 8],
     },
   ];
@@ -194,16 +190,20 @@ function Timer() {
   const handleColorBoxClick = (color) => {
     setSelectedColor(color);
     setSelectedNumber(1); // Reset selected number to default
+    handleContractMoneyChange(contractMoney, color.title);
   };
 
-  const handleContractMoneyChange = (amount) => {
+  const handleContractMoneyChange = (
+    amount,
+    colorTitle = selectedColor?.title
+  ) => {
     setContractMoney(amount);
     // Calculate possible payout when contract money changes
-    if (selectedColor) {
+    if (colorTitle) {
       let multiplier = 0;
-      if (selectedColor.title === "Red" || selectedColor.title === "Green") {
+      if (colorTitle === "Red" || colorTitle === "Green") {
         multiplier = 2;
-      } else if (selectedColor.title === "Violet") {
+      } else if (colorTitle === "Violet") {
         multiplier = 4.5;
       }
 
@@ -271,19 +271,23 @@ function Timer() {
         return "bg-gray-300"; // Default color if the winner's color is not recognized
     }
   };
- 
+
   const generateRandomBets = () => {
     const newBets = [];
     const colors = ["Red", "Violet", "Green"];
     const amounts = [100, 200, 500, 1000];
-  
+
     for (let i = 0; i < 30; i++) {
       const randomColor = colors[Math.floor(Math.random() * colors.length)];
       const randomAmount = amounts[Math.floor(Math.random() * amounts.length)];
       const randomUserNumber = Math.floor(1000 + Math.random() * 9000); // Generate a random 4-digit user number
-      newBets.push({ color: randomColor, amount: randomAmount, userNumber: randomUserNumber });
+      newBets.push({
+        color: randomColor,
+        amount: randomAmount,
+        userNumber: randomUserNumber,
+      });
     }
-  
+
     setNewBets(newBets);
   };
   return (
@@ -309,46 +313,47 @@ function Timer() {
           </div>
           <div>
             <p className="text-l">Count Down</p>
-            <div className="rounded-lg p-3 shadow-lg h-10 items-center flex justify-center">
-              <h2 className="text-3xl font-mono">{formatTime(time)}</h2>
+            <div className="rounded-lg p-3 shadow-lg h-8 items-center flex justify-center bg-white">
+              <h2 className="text-2xl font-mono">{formatTime(time)}</h2>
             </div>
           </div>
         </div>
         {/* Color Boxes */}
         <div className="p-2 mt-2 bg-gray-800 flex justify-around flex-wrap ">
           {colorBoxes.map((colorBox) => (
-            <div
-              key={colorBox.color}
-              className={`flex flex-col justify-center items-center w-1/4 border border-myblue-200 rounded-lg p-2 cursor-pointer ${
-                isDisabled ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-              onClick={() => !isDisabled && handleColorBoxClick(colorBox)}
-            >
-              <div className="text-4xl">{colorBox.icon}</div>
-              <div className="mt-2">{colorBox.title}</div>
+            <div className=" w-1/4">
+              <div
+                key={colorBox.color}
+                className={`flex flex-col justify-center items-center border-2 border-myblue-200 rounded-lg p-2 cursor-pointer bg-white shadow shadow-lg ${
+                  isDisabled ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+                onClick={() => !isDisabled && handleColorBoxClick(colorBox)}
+              >
+                <div className="text-4xl">{colorBox.icon}</div>
+                <div className="mt-2">{colorBox.title}</div>
+              </div>
+              <div className=" ml-7">{colorBox.ratio}</div>
             </div>
           ))}
         </div>
       </div>
       {/* Popup Modal */}
       <Popup
-        open={!!selectedColor}
+        open={!!selectedColor && time > 11}
         closeOnDocumentClick
         onClose={closePopup}
         modal
       >
-        <div className="modal bg-white rounded-lg p-4 shadow-lg max-w-xs mx-auto">
+        <div className="modal bg-white rounded-lg p-4 shadow-lg max-w-xs mx-auto border-2 border-myblue-200 ">
           <div className="flex justify-between items-center mb-4">
-            <h2 className="text-xl font-bold">
-              {selectedColor?.title} Numbers
-            </h2>
-            <button onClick={closePopup} className="text-red-500">
+            <h2 className="text-xl font-bold">{selectedColor?.title}</h2>
+            <button onClick={closePopup}>
               <RxCross1 />
             </button>
           </div>
           {/* User Balance */}
           <div className="mb-4">
-            <p>{`User Balance: ${user.balance}`}</p>
+            <p>{` Balance: ${user.balance}`}</p>
           </div>
           {/* Error Message */}
           {errorMessage && (
@@ -378,7 +383,7 @@ function Timer() {
                 contractMoney < 10 ||
                 user.balance < contractMoney * selectedNumber
               } // Disable if balance is insufficient
-              className={`bg-blue-500 p-2 rounded-lg w-full ${
+              className={`bg-myblue-200 p-2 rounded-lg w-full shadow shadow-lg text-white ${
                 contractMoney < 10 ||
                 user.balance < contractMoney * selectedNumber
                   ? "opacity-50 cursor-not-allowed"
@@ -392,7 +397,7 @@ function Timer() {
       </Popup>
 
       {/* WINNER DIVISION */}
-      <div className="flex flex-row justify-around border border-myblue-200 shadow shadow-lg h-14 items-center mx-4 rounded-xl">
+      <div className="flex flex-row justify-around border-2 border-myblue-200 shadow shadow-lg h-14 items-center mx-4 rounded-xl bg-white">
         <div>{lastPeriodData ? lastPeriodData.periodNumber : "Loading..."}</div>
         <div>{lastPeriodData ? lastPeriodData.color : "Loading..."}</div>
         {lastPeriodData && (
@@ -405,20 +410,29 @@ function Timer() {
       </div>
       {showRandomBets && (
         <div className="flex p-2 bg-gray-800 flex-col">
-          <EveryOneOrder key={refresh} period={formatPeriod(period)} newBets={newBets} />
+          <EveryOneOrder
+            key={refresh}
+            period={formatPeriod(period)}
+            newBets={newBets}
+          />
+          <hr></hr>
         </div>
       )}
 
       {/* Last Table Data */}
-      {time <= 11 ? (
-        <div className="flex p-2 flex-col w-[80%] mr-4 ml-4 justify-center items-center h-[150px] border border-myblue-200 mt-11">
-          <h2 className="text-myblue-200">WAIT FOR RESULT......</h2>
+      {time <= 10 ? (
+        <div className="flex p-2 flex-col mr-4 ml-4 justify-center items-center h-[150px] border-2 border-myblue-200 mt-11 shadow shadow-lg bg-white">
+          <h2 className="text-myblue-200 font-bold">WAIT FOR RESULT......</h2>
         </div>
+        
       ) : (
         <div className="flex p-2 bg-gray-800 flex-col">
           {/* <EveryOneOrder key={refresh} period={formatPeriod(period - 1)} newBets={lastTableData} /> */}
         </div>
-      )}
+      )
+      
+      }
+      
     </div>
   );
 }
