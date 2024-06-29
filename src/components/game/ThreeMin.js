09e6c2b507de5111c6ce0265278e8d01bf8a09f5
Akansh Agarwal <66241705+akansh-agarwalalwar@users.noreply.github.com>
2024-06-29 10:41:28 +0530

    Update ThreeMin.js

--- src/components/game/ThreeMin.js
+++ src/components/game/ThreeMin.js
@@ -23,7 +23,7 @@ function TwoMin() {
   const [refresh, setRefresh] = useState(0);
   const [possiblePayout, setPossiblePayout] = useState({
     Red: 19.6,
-    Violet: 45.0,
+    Violet: 44.1,
     Green: 19.6,
   });
   const [lastPeriodData, setLastPeriodData] = useState(null);
@@ -171,16 +171,20 @@ function TwoMin() {
   const handleColorBoxClick = (color) => {
     setSelectedColor(color);
     setSelectedNumber(1); // Reset selected number to default
+    handleContractMoneyChange(contractMoney, color.title);
   };
 
-  const handleContractMoneyChange = (amount) => {
+  const handleContractMoneyChange = (
+    amount,
+    colorTitle = selectedColor?.title
+  ) => {
     setContractMoney(amount);
     // Calculate possible payout when contract money changes
-    if (selectedColor) {
+    if (colorTitle) {
       let multiplier = 0;
-      if (selectedColor.title === "Red" || selectedColor.title === "Green") {
+      if (colorTitle === "Red" || colorTitle === "Green") {
         multiplier = 2;
-      } else if (selectedColor.title === "Violet") {
+      } else if (colorTitle === "Violet") {
         multiplier = 4.5;
       }
 
@@ -258,11 +262,23 @@ function TwoMin() {
   
     setNewBets(newBets);
   };
+  const getColorClass = (color) => {
+    switch (color.toLowerCase()) {
+      case "red":
+        return "bg-red-100";
+      case "green":
+        return "bg-green-100";
+      case "violet":
+        return "bg-purple-100";
+      default:
+        return "bg-gray-300"; // Default color if the winner's color is not recognized
+    }
+  };
 
   return (
     <div className="flex flex-col bg-gray-900 min-h-screen">
       {/* Header */}
-      <div className="flex flex-row justify-between bg-richblue-500 w-full text-white items-center h-12 md:h-8">
+      <div className="flex flex-row justify-between bg-myblue-200 w-full text-white items-center h-12 md:h-8">
         <Link to="/home">
           <FaArrowLeftLong className="mx-2" />
         </Link>
@@ -282,17 +298,18 @@ function TwoMin() {
           </div>
           <div>
             <p className="text-l">Count Down</p>
-            <div className="rounded-lg p-3 shadow-lg h-10 items-center flex justify-center">
-              <h2 className="text-3xl font-mono">{formatTime(time)}</h2>
+            <div className="rounded-lg p-3 shadow-lg h-8 items-center flex justify-center bg-white">
+              <h2 className="text-2xl font-mono">{formatTime(time)}</h2>
             </div>
           </div>
         </div>
         {/* Color Boxes */}
         <div className="p-2 mt-4 bg-gray-800 flex justify-around flex-wrap">
           {colorBoxes.map((colorBox) => (
+            <div className=" w-1/4">
             <div
               key={colorBox.color}
-              className={`flex flex-col justify-center items-center w-1/4 border border-gray-600 rounded-lg p-2 cursor-pointer ${
+              className={`flex flex-col justify-center items-center border-2 border-myblue-200 rounded-lg p-2 cursor-pointer bg-white shadow shadow-lg ${
                 isDisabled ? "opacity-50 cursor-not-allowed" : ""
               }`}
               onClick={() => !isDisabled && handleColorBoxClick(colorBox)}
@@ -300,25 +317,27 @@ function TwoMin() {
               <div className="text-4xl">{colorBox.icon}</div>
               <div className="mt-2">{colorBox.title}</div>
             </div>
-          ))}
-        </div>
+            <div className=" ml-7">{colorBox.ratio}</div>
+          </div>
+        ))}
+      </div>
       </div>
 
       {/* Popup Modal */}
-      <Popup open={!!selectedColor} closeOnDocumentClick onClose={closePopup} modal>
-        <div className="modal bg-white rounded-lg p-4 shadow-lg max-w-xs mx-auto">
+      <Popup open={!!selectedColor && time>11} closeOnDocumentClick onClose={closePopup} modal>
+        <div className="modal bg-white rounded-lg p-4 shadow-lg max-w-xs mx-auto border-2 border-myblue-200">
           <div className="flex justify-between items-center mb-4">
-            <h2 className="text-xl font-bold">{selectedColor?.title} Numbers</h2>
+            <h2 className="text-xl font-bold">{selectedColor?.title}</h2>
             <button onClick={closePopup} className="text-red-500">
               <RxCross1 />
             </button>
           </div>
           {/* User Balance */}
           <div className="mb-4">
-            <p>{`User Balance: ${user.balance}`}</p>
+            <p>{`Balance: ${user.balance}`}</p>
           </div>
           {/* Error Message */}
-          {errorMessage && <div className="text-red-500 mb-4">{errorMessage}</div>}
+          {errorMessage && <div className="text-red-100 mb-4">{errorMessage}</div>}
           {/* Input Field for Amount */}
           <div className="mb-4">
             <label htmlFor="amountInput">Enter Amount:</label>
@@ -332,7 +351,7 @@ function TwoMin() {
             />
           </div>
           {/* Possible Payout */}
-          <div className="mb-4">
+          <div className="mb-4 text-sm">
             <p>Possible Payout: {winAmount.toFixed(2)}</p>
           </div>
           {/* Confirm Button */}
@@ -340,7 +359,7 @@ function TwoMin() {
             <button
               onClick={handleConfirm}
               disabled={contractMoney < 10}
-              className={`bg-blue-500 p-2 rounded-lg w-full ${
+              className={`bg-myblue-200 p-2 rounded-lg w-full shadow shadow-lg text-white ${
                 contractMoney < 10 ? "opacity-50 cursor-not-allowed" : ""
               }`}
             >
@@ -351,27 +370,31 @@ function TwoMin() {
       </Popup>
 
       {/* WINNER DIVISION */}
-      <div className="flex flex-row justify-between ml-4 mr-4">
-        <div>
-          {lastPeriodData ? lastPeriodData.periodNumber : "Loading..."}
-        </div>
-        <div>
-         {lastPeriodData ? lastPeriodData.color : "Loading..."}
-        </div>
+      <div className="flex flex-row justify-around border-2 border-myblue-200 shadow shadow-lg h-14 items-center mx-4 rounded-xl bg-white">
+        <div>{lastPeriodData ? lastPeriodData.periodNumber : "Loading..."}</div>
+        <div>{lastPeriodData ? lastPeriodData.color : "Loading..."}</div>
+        {lastPeriodData && (
+          <div
+            className={`w-8 h-8 rounded-full ${getColorClass(
+              lastPeriodData.color
+            )}`}
+          ></div>
+        )}
       </div>
 
       {/* EveryOneOrder Component */}
       {showRandomBets && (
         <div className="flex p-2 bg-gray-800 flex-col">
           <TwoMinOrder key={refresh} period={formatPeriod(period)} newBets={newBets} />
+          <hr/>
         </div>
       )}
 
       {/* Last Table Data */}
       {time <= 11 ? (
-        <div className="flex p-2 flex-col w-[80%] mr-4 ml-4 justify-center items-center h-[150px] border border-myblue-200 mt-11">
-          <h2 className="text-myblue-200">WAIT FOR RESULT......</h2>
-        </div>
+        <div className="flex p-2 flex-col mr-4 ml-4 justify-center items-center h-[150px] border-2 border-myblue-200 mt-11 shadow shadow-lg bg-white">
+        <h2 className="text-myblue-200 font-bold">WAIT FOR RESULT......</h2>
+      </div>
       ) : (
         <div className="flex p-2 bg-gray-800 flex-col">
           {/* <EveryOneOrder key={refresh} period={formatPeriod(period - 1)} newBets={lastTableData} /> */}
